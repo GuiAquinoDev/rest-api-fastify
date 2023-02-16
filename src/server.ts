d23addc98ba626733e3f3ccec64093560bdd165e
Guilherme Aquino <guiiaquino92@gmail.com>
2023-02-16 20:28:08 -0300

    feat: realizando queries com knex

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import fastify from "fastify";
-import { knex } from "./database";
+import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
-    
-    return tables
+  const transactions = await await knex('transactions').select('*')
+
+  return transactions
 })
 
-app.listen({
-    port: 3333
-}).then(()=> console.log('Http Server Running!'))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('Http Server Running!'))
